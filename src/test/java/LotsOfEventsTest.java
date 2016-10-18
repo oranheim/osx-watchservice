5bfd6ad5aa68557bf06fc47f7b45e9416a5013ed
oranheim <oranheim@gmail.com>
2016-10-18 22:31:10 +0200

    added LotsOfEventsTest

---
+++ src/test/java/LotsOfEventsTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created by oranheim on 18/10/2016.
+ */
+public class LotsOfEventsTest {
+
+    @Test
+    public void testLotsOfEvents() throws Exception {
+        LotsOfEvents lots = new LotsOfEvents();
+        lots.main(null);
+    }
+}
